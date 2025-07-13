243a5a43698c27ad0633fd3dccaa5c4b75a0a593
manaswi-patil <imanaswi.patil@gmail.com>
2025-07-13 16:44:18 +0530

    update package.json file.

--- portfoli0-using-react/vite.config.js
+++ portfoli0-using-react/vite.config.js
@@ -1,25 +1,10 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  root: "./frontend",
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
-  server: {
-    historyApiFallback: true,
-  },
-  preview: {
-    historyApiFallback: true,
+    outDir: "../dist",
   },
 });
