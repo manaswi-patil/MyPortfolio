0e038eb604763fddd0ba9321a8d41d35fdbcd948
manaswi-patil <imanaswi.patil@gmail.com>
2025-07-13 16:22:01 +0530

    vercel file and vite configue file shift in root directory.

--- portfoli0-using-react/frontend/src/pages/About.jsx
+++ portfoli0-using-react/frontend/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { PenTool, Code2, Settings, ChevronDown, Sparkles } from "lucide-react";
 
 export default function AboutMeSection() {
